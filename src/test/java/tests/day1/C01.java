364327dc0567dc52852918694bbfc568ba91535b
Ahmet Emre <ahmet.emre20042004@gmail.com>
2023-03-03 12:03:04 +0100

    POM

--- src/test/java/tests/day1/C01.java
+++ src/test/java/tests/day1/C01.java
@@ -1,4 +1,30 @@
 package tests.day1;
 
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import page.AmazonPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
 public class C01 {
+    AmazonPage amazonPage=new AmazonPage();
+
+    @Test
+    public void amazonTest01(){
+        Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+        Driver.getDriver().navigate().refresh();
+        String expected="amazon";
+        String actual=Driver.getDriver().getCurrentUrl();
+        Assert.assertTrue(actual.contains(expected));
+        amazonPage.aramaKutusu.sendKeys("ipad"+ Keys.ENTER);
+        expected="1-16 of 995 results for \"ipad\"";
+        actual=amazonPage.aramaSonucElementi.getText();
+        Assert.assertTrue(actual.contains(expected));
+
+        amazonPage.angebotDesTages.click();
+
+
+    }
+
 }
